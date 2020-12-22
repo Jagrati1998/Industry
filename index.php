7ec9d976cff6bb73427f74f86fe07fbb6f9fc0a2
Jagrati Mishra <jagratimishra2222@gmail.com>
2020-12-22 23:38:53 +0530

    kkk

---
+++ index.php
@@ -0,0 +1,337 @@
+<html>
+<head>
+    <title> Industry Name</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Scholarly web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript">
+         addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!--// Meta tag Keywords -->
+    <!-- css files -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/sandstone/bootstrap.min.css">
+    <link rel="stylesheet" href="css/mycss.css">
+	<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
+
+    <!-- online-fonts -->
+    <!-- //online-fonts -->
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#"><div class="navfont">Navbar</div></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+
+                    <a class="nav-link" style="font-size:20px" href="#">
+                        Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="font-size:20px">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="font-size:20px">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="font-size:20px">About</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="font-size:20px">Dropdown</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" style="width:200px;height:40px;font-size:15px " placeholder="Search">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+    <div class="image_container">
+  <img src="images/carousel1.jpg" alt="Notebook" style="width:100%;">
+  <div class="content">
+    <h1>Hello Welcome To New Site</h1>
+    <h4>Jagrati Mishra</h4>
+  </div>
+</div>
+
+	<div class="horizental_scrool">
+	<marquee behavior="alternate" scrolldelay="100" scrollamount="3" onmouseover="this.stop();" onmouseout="this.start();">
+                        <p class="fl_left">
+
+                            <a href="pdf/Research Facility Notice.pdf" target="_blank" rel="noopener"><font style="text-shadow: 0 0 1px;">This is important news.</font> <img width="33" height="16" src="images/new_red.gif"></a> 
+                            
+                            <a href="pdf/Research Facility Notice.pdf" target="_blank" rel="noopener"><font style="text-shadow: 0 0 1px";>This is important news.</font> <img width="33" height="16" src="images/new_red.gif"></a> 
+                    
+                            <a href="pdf/Research Facility Notice.pdf" target="_blank" rel="noopener"><font style="text-shadow: 0 0 1px";>This is important news.</font> <img width="33" height="16" src="images/new_red.gif"></a> 
+				            <a href="pdf/Research Facility Notice.pdf" target="_blank" rel="noopener"><font style="text-shadow: 0 0 1px;">This is important news.</font> <img width="33" height="16" src="images/new_red.gif"></a> 
+                            
+                            <a href="pdf/Research Facility Notice.pdf" target="_blank" rel="noopener"><font style="text-shadow: 0 0 1px";>This is important news.</font> <img width="33" height="16" src="images/new_red.gif"></a> 
+                    
+                            <a href="pdf/Research Facility Notice.pdf" target="_blank" rel="noopener"><font style="text-shadow: 0 0 1px;e";>This is important news.</font> <img width="33" height="16" src="images/new_red.gif"></a> 
+				   				   </marquee>
+					</div>
+    <div class="jumbotron">
+        <h1 class="display-3">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </div>
+	<div class="row">
+  <div class="col-sm-6">
+    
+       <div class="center">
+               
+
+                    <h2 class="center2">Admission Form</h2>
+                    <form action="/action_page.php">
+                        <label for="fname">First Name</label>
+                        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+                        <label for="lname">Last Name</label>
+                        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+                        <label for="country">Country</label>
+                        <select id="country" name="country">
+                            <option value="australia">Australia</option>
+                            <option value="canada">Canada</option>
+                            <option value="usa">USA</option>
+                        </select>
+
+                        <input type="submit" value="Submit">
+                    </form>
+                </div>			</div>
+				
+
+
+<div class="col-sm-6">
+    
+       <div class="center2">
+	   
+              <div class="noticepanel">
+			  <div class="notice"><h1 class="center2">Important Notice</h1></div>
+			  <div class="myscroolbar">
+			  <marquee direction ="up"scrolldelay="50"onmouseover="this.stop();" onmouseout="this.start();">
+			  <ul><img width="33" height="16" src="images/new_red.gif"><a class="two" href="default.asp" target="_blank">This link changes font-size</a></ul>
+			  <ul><img width="33" height="16" src="images/new_red.gif"><a class="two" href="default.asp" target="_blank">This link changes font-size</a></ul>
+			  <ul><img width="33" height="16" src="images/new_red.gif"><a class="two" href="default.asp" target="_blank">This link changes font-size</a></ul>
+			  <ul><img width="33" height="16" src="images/new_red.gif"><a class="two" href="default.asp" target="_blank">This link changes font-size</a></ul>
+
+			  <ul><img width="33" height="16" src="images/new_red.gif"><a class="two" href="default.asp" target="_blank">This link changes font-size</a></ul>
+			  <ul><img width="33" height="16" src="images/new_red.gif"><a class="two" href="default.asp" target="_blank">This link changes font-size</a></ul>
+			  <ul><img width="33" height="16" src="images/new_red.gif"><a class="two" href="default.asp" target="_blank">This link changes font-size</a></ul>
+			  <ul><img width="33" height="16" src="images/new_red.gif"><a class="two" href="default.asp" target="_blank">This link changes font-size</a></ul>
+
+				
+				  </marquee>
+			 
+			  </div>
+			  </div>
+                   
+</div>
+</div>
+</div>
+
+
+			<div class=whole_image>
+       
+		<div class="row">
+		
+		<div class="col-sm-3">
+		<div class="imagecenter">
+    
+        <img id="myImg1" src="images/g1.jpg" alt="Snow" style="width:100%;max-width:300px">
+		</div>
+		</div>
+
+    
+      <div class="col-sm-3">
+    
+		<div class="imagecenter">
+
+    
+        <img id="myImg2" src="images/g1.jpg" alt="Snow" style="width:100%;max-width:300px">
+		</div>
+		</div>
+
+   <div class="col-sm-3">
+    
+		<div class="imagecenter">
+
+    
+        <img id="myImg3" src="images/g1.jpg" alt="Snow" style="width:100%;max-width:300px">
+		</div>
+		</div>
+		
+		
+
+    
+        
+		
+		
+   <div class="col-sm-3">
+    
+		<div class="imagecenter">
+
+    
+        <img id="myImg4" src="images/g1.jpg" alt="Snow" style="width:100%;max-width:300px">
+		</div>
+		</div>
+		
+		
+
+    
+        
+		</div>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+  <span class="close">&times;</span>
+  <img class="modal-content" id="img01">
+  <div id="caption"></div>
+</div>
+
+
+		<div class="row">
+		
+		<div class="col-sm-3">
+		<div class="imagecenter">
+    
+        <img id="myImg1" src="images/g1.jpg" alt="Snow" style="width:100%;max-width:300px">
+		</div>
+		</div>
+
+    
+      <div class="col-sm-3">
+    
+		<div class="imagecenter">
+
+    
+        <img id="myImg2" src="images/g1.jpg" alt="Snow" style="width:100%;max-width:300px">
+		</div>
+		</div>
+
+   <div class="col-sm-3">
+    
+		<div class="imagecenter">
+
+    
+        <img id="myImg3" src="images/g1.jpg" alt="Snow" style="width:100%;max-width:300px">
+		</div>
+		</div>
+		
+		
+
+    
+        
+		
+		
+   <div class="col-sm-3">
+    
+		<div class="imagecenter">
+
+    
+        <img id="myImg4" src="images/g1.jpg" alt="Snow" style="width:100%;max-width:300px">
+		</div>
+		</div>
+		
+		
+
+    
+        
+		</div>
+
+		</div>
+		
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+  <span class="close">&times;</span>
+  <img class="modal-content" id="img01">
+  <div id="caption"></div>
+</div>
+
+
+       <script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img1 = document.getElementById("myImg1");
+var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
+var img4 = document.getElementById("myImg4");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img1.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img2.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img3.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img4.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+</script>
+<div id="googleMap" style="width:100%;height:400px;"></div>
+
+<script>
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
+
+</body>
+</html>
